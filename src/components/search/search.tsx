6190a30ec988016603d197dc519516623cad6f7b
Mihaylik <ki201_mdv@student.ztu.edu.ua>
2023-02-23 02:20:22 +0200

    added bolded in search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,30 +1,36 @@
-
-import React, { useState } from 'react';
-import { AutoComplete, Input } from 'antd';
-import {Link, useLocation} from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import {PokemonList, PokemonShortInfo} from "../../store/types/pokemonDataTypes";
+import React, {useState} from 'react';
+import {AutoComplete, Input} from 'antd';
+import {useNavigate} from 'react-router-dom';
+import {PokemonShortInfo} from "../../store/types/pokemonDataTypes";
 
 interface SearchProps {
     pokemonList: PokemonShortInfo[];
 }
 
-const { Option } = AutoComplete;
-
-const Search: React.FC<SearchProps> = ({ pokemonList }) => {
+const Search: React.FC<SearchProps> = ({pokemonList}) => {
     const navigation = useNavigate();
     const [options, setOptions] = useState<any>([]);
 
+    const getBoldedVariant = (charPrinted: number, variant: string): JSX.Element => {
+        if (charPrinted > variant.length)
+            return <b>{variant}</b>
+        let bold = variant.slice(0, charPrinted)
+        let common = variant.slice(charPrinted, variant.length)
+        return <><b>{bold}</b>{common}</>
+    }
+
     const handleSearch = (value: string) => {
         const filteredList = pokemonList.filter((pokemon) =>
-            pokemon.name.includes(value.toLowerCase())
+            pokemon.name.indexOf(value.toLowerCase()) === 0
         );
 
         setOptions(
-            filteredList.map((pokemon) => ({
-                value: pokemon.name,
-                label: pokemon.name,
-            }))
+            filteredList.map((pokemon) => {
+                return {
+                    value: pokemon.name,
+                    label: getBoldedVariant(value.length, pokemon.name),
+                }
+            })
         );
     };
 
@@ -49,7 +55,7 @@ const Search: React.FC<SearchProps> = ({ pokemonList }) => {
     return (
         <AutoComplete
             options={options}
-            style={{ width: 200 }}
+            style={{width: 200}}
             onSelect={onSelect}
             onSearch={handleSearch}
             filterOption={(inputValue, option) =>
